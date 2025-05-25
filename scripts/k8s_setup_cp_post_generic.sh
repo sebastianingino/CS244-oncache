b9d30e85f3118e825d37561ff402f4904298e669
Sebastian Ingino <ingino@stanford.edu>
2025-05-25 07:26:03 +0000

    update script

--- scripts/k8s_setup_cp_post_generic.sh
+++ scripts/k8s_setup_cp_post_generic.sh
@@ -5,7 +5,7 @@ CIDR=10.244.0.0/16
 
 # Control plane setup script for Kubernetes
 # based on https://medium.com/@priyantha.getc/step-by-step-guide-to-creating-a-kubernetes-cluster-on-ubuntu-22-04-using-containerd-runtime-0ead53a8d273
-sudo kubeadm config images pull --cri-socket unix:///var/run/containerd/containerd.sock
+# sudo kubeadm config images pull --cri-socket unix:///var/run/containerd/containerd.sock
 echo "KUBELET_EXTRA_ARGS=--node-ip=$LOCAL_IP" | sudo tee /etc/default/kubelet
 sudo systemctl restart kubelet.service
 
