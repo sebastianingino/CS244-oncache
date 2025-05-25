6b10b6cd4098ae9998e0ddeee56127afa4dedd95
Sebastian Ingino <ingino@stanford.edu>
2025-05-25 05:24:32 +0000

    Update cilium instalation

--- scripts/cilium_install.sh
+++ scripts/cilium_install.sh
@@ -1,10 +1,20 @@
-CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
-CLI_ARCH=amd64
-if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
-curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
-sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
-sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
-rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+#!/bin/bash
 
-# Install
-cilium install --version 1.17.3
+# Install helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+# Install cilium
+helm install cilium cilium/cilium --version 1.17.4 \
+--namespace kube-system \
+--set routingMode=native \
+--set bpf.masquerade=true \
+--set ipv4.enabled=true \
+--set enableIPv4BIGTCP=true \
+--set installNoConntrackIptablesRules=true \
+--set kubeProxyReplacement=true \
+--set ipv4NativeRoutingCIDR="10.244.0.0/16" \
+--set bpf.distributedLRU.enabled=true 
