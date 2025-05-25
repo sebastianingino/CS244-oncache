66433ff9aaefbbc6205991b45aecaf3b0b8e393a
ingino <ingino@node0.ingino-258380.cs244-oncache-pg0.utah.cloudlab.us>
2025-05-25 03:14:49 +0000

    Update setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install iperf3 netperf clang libbpf-dev llvm make -y
+sudo apt install iperf3 netperf clang libbpf-dev llvm make gh -y
 
 sudo ln -s /usr/include/x86_64-linux-gnu/asm /usr/include/asm
 
 curl https://mise.run | sh
+
+echo "eval \"\$(/users/ingino/.local/bin/mise activate bash)\"" >> ~/.bashrc
+
+source ~/.bashrc
