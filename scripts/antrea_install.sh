d8a09d80c884fd1e2a4cc3216da54e7c3091d739
Sebastian Ingino <ingino@stanford.edu>
2025-05-26 00:52:02 -0700

    Update antrea version

--- scripts/antrea_install.sh
+++ scripts/antrea_install.sh
@@ -1,1 +1,1 @@
-kubectl apply -f https://github.com/antrea-io/antrea/releases/download/v2.2.1/antrea.yml
+kubectl apply -f https://github.com/antrea-io/antrea/releases/download/v2.3.0/antrea.yml
--- scripts/antrea_uninstall.sh
+++ scripts/antrea_uninstall.sh
@@ -1,1 +1,1 @@
-kubectl delete -f https://github.com/antrea-io/antrea/releases/download/v2.2.1/antrea.yml
+kubectl delete -f https://github.com/antrea-io/antrea/releases/download/v2.3.0/antrea.yml
