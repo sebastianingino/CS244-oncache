334460e1f845ee54962f409f8299d0d949ce019b
Sebastian Ingino <ingino@stanford.edu>
2025-05-27 20:51:32 -0700

    Reset cilium script

--- scripts/cilium_install_1-16.sh
+++ scripts/cilium_install_1-16.sh
@@ -11,4 +11,15 @@ sudo apt-get install helm
 helm repo add cilium https://helm.cilium.io/
 helm install cilium cilium/cilium --version 1.16.10 \
 --namespace kube-system \
---set hubble.enabled=false
+--set kubeProxyReplacement=true \
+--set k8sServiceHost="10.10.1.1" \
+--set k8sServicePort=6443 \
+--set routingMode=native \
+--set bpf.masquerade=true \
+--set ipv4.enabled=true \
+--set enableIPv4BIGTCP=true \
+--set ipv4NativeRoutingCIDR="10.244.0.0/16" \
+--set autoDirectNodeRoutes=true \
+--set installNoConntrackIptablesRules=true \
+--set hubble.enabled=false \
+--set bpfClockProbe=true
