513aad09ed524f3c65e969eeac0b4cead0c7bae6
Sebastian Ingino <ingino@stanford.edu>
2025-05-30 20:46:54 +0000

    Make this work on u20 ootb

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,10 @@
 sudo apt update
-sudo apt install iperf3 netperf clang libbpf-dev llvm make gh linux-tools-common linux-tools-generic linux-tools-$(uname -r) -y
+sudo apt install iperf3 netperf clang libbpf-dev llvm make linux-tools-common linux-tools-generic linux-tools-$(uname -r) -y
 
 sudo ln -s /usr/include/x86_64-linux-gnu/asm /usr/include/asm
+
+curl -sS https://webi.sh/gh | sh
+source ~/.config/envman/PATH.env
 
 curl https://mise.run | sh
 
