a24d8a6ac8009b34d7c0df59038543737c162409
ingino <ingino@localhost>
2025-05-31 19:40:31 -0600

    Update setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,12 @@
 sudo apt update
-sudo apt install iperf3 netperf clang libbpf-dev llvm make linux-tools-common linux-tools-generic linux-tools-$(uname -r) -y
+sudo apt install iperf3 clang libbpf-dev llvm make linux-tools-common linux-tools-generic linux-tools-$(uname -r) -y
 
 sudo ln -s /usr/include/x86_64-linux-gnu/asm /usr/include/asm
+
+sudo apt install -y wget build-essential && wget https://github.com/HewlettPackard/netperf/archive/netperf-2.7.0.tar.gz && tar -zxvf netperf-2.7.0.tar.gz && (cd netperf-netperf-2.7.0/ && ./configure && make && sudo make install)
+
+rm netperf-2.7.0.tar.gz
+rm -rf netperf-netperf-2.7.0/
 
 curl -sS https://webi.sh/gh | sh
 source ~/.config/envman/PATH.env
