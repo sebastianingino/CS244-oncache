416ccdc6bb2707c9fa31abd0c825dc1563e724ee
Sebastian Ingino <ingino@stanford.edu>
2025-06-06 15:48:09 -0700

    Add a setup script for smaller clusters

---
+++ scripts/post_k8s_setup_small.sh
@@ -0,0 +1,6 @@
+# Remove taint
+kubectl taint nodes node0 node-role.kubernetes.io/master:NoSchedule-
+
+# Add roles
+kubectl label nodes node0 bench-role=client
+kubectl label nodes node1 bench-role=server
