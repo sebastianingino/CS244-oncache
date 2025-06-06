dc0a17d4c592d210a478b8df5986d96d4dcdf833
Sebastian Ingino <ingino@stanford.edu>
2025-06-05 23:20:06 -0700

    Spacing

--- scripts/k8s_reset.sh
+++ scripts/k8s_reset.sh
@@ -2,6 +2,7 @@ sudo rm -f /etc/containerd/config.toml
 sudo kubeadm reset -f; rm $HOME/.kube/config -f
 sudo rm -rf /etc/cni/net.d
 sudo ipvsadm --clear
+
 sudo rm -rf /var/lib/etcd
 sudo rm -rf /var/lib/cni
 sudo rm -rf /var/lib/kubelet
