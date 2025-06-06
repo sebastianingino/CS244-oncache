9c6d6c0f0025455055433728d71e39c682559149
Sebastian Ingino <ingino@stanford.edu>
2025-06-05 23:15:54 -0700

    Fix reset script

--- scripts/k8s_reset.sh
+++ scripts/k8s_reset.sh
@@ -1,5 +1,10 @@
-sudo rm -f /etc/containerd/config.toml; sudo systemctl restart containerd.service
-sudo systemctl restart kubelet.service
+sudo rm -f /etc/containerd/config.toml
 sudo kubeadm reset -f; rm $HOME/.kube/config -f
 sudo rm -rf /etc/cni/net.d
 sudo ipvsadm --clear
+sudo rm -rf /var/lib/etcd
+sudo rm -rf /var/lib/cni
+sudo rm -rf /var/lib/kubelet
+
+sudo systemctl restart containerd.service
+sudo systemctl restart kubelet.service
